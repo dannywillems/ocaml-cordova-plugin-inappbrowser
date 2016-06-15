63eeb780229cb04707d2496b9f4565c95a01b68f
Danny Willems <contact@danny-willems.be>
2016-06-15 03:06:04 +0200

    Remove test

--- test/test.ml
+++
@@ -1,6 +0,0 @@
-let i = Cordova_in_app_browser.t () in
-(* Clear cache and show InAppBrowser location bar *)
-let opt = Cordova_in_app_browser.options_list_to_str [Cordova_in_app_browser.location true ;
-Cordova_in_app_browser.ai_clear_cache true] in
-(* Opens in the Cordova WebView if the URL is in the white list *)
-i#open_ "https://ocaml.org" (Cordova_in_app_browser.target_self) opt
